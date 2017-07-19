36566ef4de024006033230be1749d5254376e6ba
TPL <trphloc@gmail.com>
2017-07-19 15:30:17 +0700

    - show QRCode to imageView

--- app/src/main/java/com/ygaps/generateqrcode/MainActivity.java
+++ app/src/main/java/com/ygaps/generateqrcode/MainActivity.java
@@ -20,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        try {
+            Bitmap bitmap = encodeAsBitmap("Truong Phuoc Loc, QRCode");
+            imageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
     Bitmap encodeAsBitmap(String str) throws WriterException {
         BitMatrix result;
         try {
